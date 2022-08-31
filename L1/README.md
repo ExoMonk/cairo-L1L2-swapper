43d234ecc388fc5d9762d9f9b7fa515d005728b4
0xExoMonk <0xexomonk@gmail.com>
2022-08-31 12:04:53 +0200

    L1Swapper structure contracté

--- L1/README.md
+++ L1/README.md
@@ -1,17 +1,13 @@
 # L1 Locking contract
 
-
-Try running some of the following tasks:
+Setup : 
 
 ```shell
+npm install
+
 npx hardhat help
 npx hardhat test
 GAS_REPORT=true npx hardhat test
 npx hardhat node --network localhost
 npx hardhat run scripts/deploy.js --network localhost
 ```
-
-
-@nomiclabs/hardhat-waffle
-dotenv
-hardhat
---
+++ L1/contracts/L1Swapper.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+import "./IStarknetCore.sol";
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+enum TokenType {
+    WETH,
+    DAI,
+    USDC
+}
+
+
+contract L1Swapper is Ownable {
+
+    ///////////////
+    /// Storage ///
+    ///////////////
+
+    //Contracts
+    IStarknetCore immutable public starknetCore;
+    ISwapRouter immutable public swapRouter;
+    address public l2_contract;
+
+    //ERC20
+    IERC20 immutable public usdc;
+    IERC20 immutable public weth;
+
+    event receivedAmount(address sender, uint amount);
+
+    constructor(
+        IStarknetCore starknetCore_,
+        ISwapRouter swapRouter_,
+        address l2_contract_,
+        IERC20 usdc_, 
+        IERC20 weth_
+    ) payable {
+        require(address(starknetCore_) != address(0));
+        swapRouter = swapRouter_;
+        starknetCore = starknetCore_;
+        l2_contract = l2_contract_;
+
+        usdc = usdc_;
+        weth = weth_;
+    }
+
+    function updateL2Contract(address l2_new_contract_) public onlyOwner {
+        l2_contract = l2_new_contract_;
+    }
+
+
+    function swap_token() public onlyOwner {
+
+    }
+}
