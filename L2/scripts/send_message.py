128cf323f4b46aea9467950e56c6c222eed4fef0
0xExoMonk <0xexomonk@gmail.com>
2022-09-01 17:08:25 +0200

    L2 Scripts upgrade ✨

--- L2/scripts/send_message.py
+++ L2/scripts/send_message.py
@@ -13,6 +13,8 @@ CONTRACT_FILE = ['contracts/contract.cairo']
 
 OWNER = 0x07445Bd422e6B9C9cDF04E73a4Cf36Ea7C011A737795D13c9342593e789A6a33
 L2_CONTRACT = int(os.environ.get('L2_CONTRACT'), 16)
+L1_CALLER = int(os.environ.get('L1_CALLER'), 16)
+
 
 with open('./artifacts/abis/contract.json', 'r') as f:
     ABI = json.load(f)
@@ -27,7 +29,7 @@ async def send_message():
     contract = await Contract.from_address(client=acc_client, address=L2_CONTRACT)
     print("⏳ Sending Message to L1...")
     invocation = await contract.functions["swap_token_message"].invoke(
-        token_from=0, token_to=1, amount=100, max_fee=int(1e16)
+        l1_caller=L1_CALLER, token_from=0, token_to=1, amount=100, max_fee=int(1e16)
     )
     await invocation.wait_for_acceptance()
     print(f'✨ Message sent to relayer on L1 !')
